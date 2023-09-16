4cdda3b4e5c1cecb1795ee78ac9cd381224310ef
rotichknovestus <noverotich@gmail.com>
2023-09-16 13:14:53 +0300

    first commit

---
+++ python1/python_print_function.py
@@ -0,0 +1,11 @@
+# print
+
+print("Hello Programmer")
+print(2)
+print(2.2)
+
+print("Hello Novestus", 2, 2.2)
+
+print("Hello kip" , "are you ready for coding?")
+print("Hello kip" + "are you ready for coding?")
+print("Hello kip" "\n" "are you ready for coding?")
